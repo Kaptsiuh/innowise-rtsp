8d273bc7dd23219f1d279fb55b4af6870b9b9c2c
Dzmitry <dimaberry1@gmail.com>
2026-06-29 14:35:22 +0300

    refactor: reorganize structure and add default query options

---
+++ src/app/entrypoint/main.tsx
@@ -0,0 +1,26 @@
+import "../styles/index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "../App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>,
+);
