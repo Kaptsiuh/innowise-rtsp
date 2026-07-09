c622e5d88755bdd591df4aba61abff2524d6cfaa
Dima Berry <dimaberry1@gmail.com>
2026-07-09 15:16:16 +0300

    Updates

---
+++ assets/_authenticated-BAyaQv8F.js
@@ -0,0 +1,1 @@
+import{C as e,_ as t,v as n}from"./index-BH0Gtnlz.js";var r=t(),i=e(),a=()=>{let e=(0,r.c)(1),t;return e[0]===Symbol.for(`react.memo_cache_sentinel`)?(t=(0,i.jsx)(n,{}),e[0]=t):t=e[0],t};export{a as component};
